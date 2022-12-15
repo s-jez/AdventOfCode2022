5edc30013aeb61c6a02256787b920204165f0e29
s-jez <stanislawjez7@gmail.com>
2022-12-15 20:46:02 +0100

    add changes.

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,5 +1,14 @@
 List<string> firstList = new List<string>();
 List<string> secondList = new List<string>();
+
+List<char> chars = new List<char>();
+List<char> chars2 = new List<char>();
+
+char[] lowerLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+char[] upperLetters = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+int totalPriority;
+
 using (var reader = new StreamReader("input.txt"))
 {
     string line;
@@ -10,4 +19,39 @@ using (var reader = new StreamReader("input.txt"))
         firstList.Add(firstCompartment);
         secondList.Add(secondCompartment);
     }
+    foreach (var item in firstList)
+    {
+        foreach(char letter in item)
+        {
+            chars.Add(letter);
+        }
+    }
+    foreach(var item in secondList)
+    {
+        foreach(char letter in item)
+        {
+            chars2.Add(letter);
+        }
+    }
+    var distinctLetters = chars.Union(chars2).ToArray();
+    totalPriority = 0;
+    for(int i = 0; i <= distinctLetters.Length - 1; i++)
+    {
+        Console.WriteLine(distinctLetters[i]);
+        for(int j=0; j <= lowerLetters.Length - 1; j++)
+        {
+            if (distinctLetters[i] == lowerLetters[j])
+            {
+                totalPriority += j;
+            }
+        }
+        for (int k = 0; k <= upperLetters.Length - 1; k++)
+        {
+            if (distinctLetters[i] == upperLetters[k])
+            {
+                totalPriority += k + 27;
+            }
+        }
+    }
+    Console.WriteLine("Sum of the priorities of item types: " + totalPriority);
 }
